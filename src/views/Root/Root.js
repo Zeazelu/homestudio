852aed8d88c44eaa7cbbf8213c0a162c8e1f1d0e
Zeazelu <zeazelu@gmail.com>
2024-02-10 11:54:16 +0100

    deploy to github pages

--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,17 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from '../../components/Header/Header';
-import ProductsView from '../ProductsView/ProductsView';
 import MainPageView from '../MainPageView/MainPageView';
 
 function Root() {
   return (
     <BrowserRouter>
       <Header />
-      <Routes>
-        <Route path='/' Component={MainPageView} exact/>
-        <Route path='/products' Component={ProductsView} />
-      </Routes>
+      <MainPageView />
     </BrowserRouter>
 
 
